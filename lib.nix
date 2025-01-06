d0895cb0d26ec71cd921b6cabe0fc75f03d5954d
Sviatoslav König <reveni324@gmail.com>
2025-01-06 20:05:48 +0300

    Update lib.nix

--- lib.nix
+++ lib.nix
@@ -1,12 +1,14 @@
-
 { pkgs }: {
-	deps = [
-		pkgs.clang
-		pkgs.ccls
-		pkgs.gdb
-		pkgs.gnumake
-		pkgs.opencv
-		pkgs.pkg-config
-		pkgs.unzip
-	];
+  deps = [
+    pkgs.clang
+    pkgs.ccls
+    pkgs.gdb
+    pkgs.gnumake
+    pkgs.opencv
+    pkgs.pkg-config
+    pkgs.unzip
+    pkgs.spdlog.dev
+    pkgs.qt6.qtbase
+    pkgs.qt6.qttools
+  ];
 }
